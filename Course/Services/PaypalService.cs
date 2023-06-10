167949c8614ee1f837c2c3084d295c23b401e37d
PauloRTrindade <prtrindadesf@gmail.com>
2023-06-10 10:58:28 -0300

    Created class 'PaypalService'

---
+++ Course/Services/PaypalService.cs
@@ -0,0 +1,20 @@
+﻿namespace Course.Services
+{
+    internal class PaypalService : IOnlinePaymentService
+    {
+        public double Interest(double amount, int months)
+        {
+            double installment = amount / months;
+            for (int i = 0; i < months; i++)
+            {
+                installment += installment * 0.01 * i++;
+            }
+            return installment;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount += amount * 0.02;
+        }
+    }
+}
